8ff4291372fb4eea3a15442e7644f44aac42c12b
Aniket Dixit <dixitaniket199@gmail.com>
2023-04-20 22:53:33 +0530

    cleanup unused files

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BINARY=wasmd
-CHAIN_DIR=./data
-CHAINID_1="wasm-test"
-
-echo "Starting $CHAINID_1 in $CHAIN_DIR..."
-echo "Creating log file at $CHAIN_DIR/$CHAINID_1.log"
-$BINARY start --log_level trace --log_format json --home $CHAIN_DIR/$CHAINID_1 --pruning=everything --minimum-gas-prices=0.00001stake > $CHAIN_DIR/$CHAINID_1.log 2>&1 &
