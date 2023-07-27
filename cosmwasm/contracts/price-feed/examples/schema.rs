853cb13c205121b8268523e467d2e0695b24e579
Aniket Dixit <dixitaniket199@gmail.com>
2023-07-28 01:32:14 +0530

    schema modifications

--- cosmwasm/contracts/price-feed/examples/schema.rs
+++ cosmwasm/contracts/price-feed/examples/schema.rs
@@ -4,7 +4,7 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use std_reference::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
-use std_reference::state::{RefData, RefMedianData, ReferenceData};
+use std_reference::state::{RefData, RefMedianData, ReferenceData,RefDeviationData};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -17,6 +17,7 @@ fn main() {
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(RefData), &out_dir);
-    export_schema(&schema_for!(ReferenceData), &out_dir);
     export_schema(&schema_for!(RefMedianData), &out_dir);
+    export_schema(&schema_for!(RefDeviationData), &out_dir);
+    export_schema(&schema_for!(ReferenceData), &out_dir);
 }
